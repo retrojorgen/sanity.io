6c6537b83c1bf9a900be9559ed10d8850d2a2372
Jørgen Jacobsen <jorgeja@gmail.com>
2025-02-26 21:24:11 +0100

    Updated menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,48 +1,112 @@
 import Image from 'next/image';
-import Logo from '../assets/sanity-monogram--red.png';
+import Logo from '../assets/sanity-monogram--red.svg';
+import Search from '../assets/search.png';
+
+// Simple interface for keeping track of the MenuItems structure
+interface MenuItems {
+  title: string;
+  items: {
+    title: string;
+    items: {
+      title: string;
+      description?: string;
+    }[];
+  }[];
+}
 
 const Header = () => (
-  <header>
-    <Image
-      className="dark:invert"
-      src={Logo}
-      alt="Sanity.io logo"
-      width={35}
-      height={35}
-      priority
-    />
-    <nav>
-      <ul>
-        {MenuItems.map((item, id) => (
-          <li key={id}>
-            <h2>{item.title}</h2>
-            <div className="sub-menu">
-              {item.items.map((subItem, id) => (
-                <div className="sub-menu-row" key={id}>
-                  <h3>{subItem.title}</h3>
-                  <ul>
-                    {subItem.items.map((subSubItem, id) => (
-                      <li key={id}>
-                        <h4>{subSubItem.title}</h4>
-                        <p>{subSubItem.description}</p>
-                      </li>
-                    ))}
-                  </ul>
+  <header className="w-full max-w-screen-xl mx-auto px-4 py-8 flex items-center justify-between">
+    <section className="flex gap-8 items-center">
+      <a href="#">
+        <Image
+          className="dark:invert"
+          src={Logo}
+          alt="Sanity.io logo"
+          width={35}
+          height={35}
+          priority
+        />
+      </a>
+      <nav id="main-navigation">
+        <ul className="flex gap-2">
+          {MenuItems.map((item, id) => (
+            <li key={id} className="group relative">
+              <h2 className="cursor-pointer text-black hover:bg-white rounded-lg p-4 block text-base group-hover:bg-white">
+                {item.title}
+              </h2>
+              {item.items?.length > 0 && (
+                <div className="sub-menu bg-background absolute hidden group-hover:flex top-full left-0 gap-8 shadow-lg p-8 rounded-lg">
+                  {item.items.map((subItem, id) => (
+                    <div className="sub-menu-row" key={id}>
+                      <h3 className="uppercase text-sm mb-4 text-black/60">
+                        {subItem.title}
+                      </h3>
+                      <ul className="flex flex-col gap-4">
+                        {subItem.items.map((subSubItem, id) => (
+                          <li key={id}>
+                            <a href="#">
+                              <h4 className="text-lg text-nowrap">
+                                {subSubItem.title}
+                              </h4>
+                              <p className="text-nowrap text-black/60">
+                                {subSubItem.description}
+                              </p>
+                            </a>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
+              )}
+            </li>
+          ))}
+          <li></li>
+        </ul>
+      </nav>
+    </section>
+    <section>
+      <nav>
+        <ul className="flex gap-4 items-center">
+          <li>
+            <button className="p-4">
+              <Image
+                src={Search}
+                alt="Search for content"
+                width={21}
+                height={21}
+              />
+            </button>
           </li>
-        ))}
-        <li></li>
-      </ul>
-    </nav>
+          <li>
+            <button className="p-4">Log in</button>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-black bg-white rounded-lg px-8 py-2 shadow-sm block"
+            >
+              Contact sales
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="text-white bg-black rounded-lg px-8 py-2 block"
+            >
+              Get started
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </section>
   </header>
 );
 
 export default Header;
 
-// should be put in Sanity datastore
-const MenuItems = [
+// Mock data, should be moved to Sanity
+const MenuItems: MenuItems[] = [
   {
     title: 'Product',
     items: [
@@ -89,5 +153,61 @@ const MenuItems = [
         ],
       },
     ],
+  },
+  {
+    title: 'Developers',
+    items: [
+      {
+        title: 'Platform',
+        items: [
+          {
+            title: 'Documentation',
+          },
+          {
+            title: 'Courses',
+          },
+          {
+            title: 'Guides',
+          },
+          {
+            title: 'API References',
+          },
+          {
+            title: 'Sanity UI',
+          },
+          {
+            title: 'GROQ cheat sheet',
+          },
+        ],
+      },
+      {
+        title: 'Build and share',
+        items: [
+          {
+            title: 'Templates',
+          },
+          {
+            title: 'Tools and plugins',
+          },
+          {
+            title: 'Schemas and snippets',
+          },
+          {
+            title: 'Project showcase',
+          },
+          {
+            title: 'Share your work',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Enterprise',
+    items: [],
+  },
+  {
+    title: 'Pricing',
+    items: [],
   },
 ];
